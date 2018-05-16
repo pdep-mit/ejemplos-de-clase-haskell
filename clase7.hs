939aedf4e7eb502b6776d650e6ce92d1768f4f41
Gustavo <gustavo.crespi@10pines.com>
2018-05-16 15:54:03 -0300

    agrego primera parte de la clase de ejercitación de Samurai Jack

--- clase7.hs
+++ clase7.hs
@@ -19,17 +19,52 @@ instance Eq Elemento where
   (==) elemento1 elemento2 = tipo elemento1 == tipo elemento2
 
 -- Punto 1
-mandarAlAnio = undefined
-meditar = undefined
-causarDanio = undefined
+mandarAlAnio :: Int -> Personaje -> Personaje
+mandarAlAnio anio (UnPersonaje nombre salud elementos _) = 
+  UnPersonaje nombre salud elementos anio
+
+mandarAlAnio' anio personaje = 
+  personaje { anioPresente = anio }  
+
+meditar :: Personaje -> Personaje
+meditar = modificarSalud (*1.5)
+
+causarDanio :: Float -> Personaje -> Personaje
+causarDanio danio = modificarSalud (max 0 . flip (-) danio)
+
+modificarSalud :: (Float -> Float) -> Personaje -> Personaje 
+modificarSalud transformacion personaje =
+  personaje { salud = (transformacion . salud) personaje }
 
 -- Punto 2
-esMalvado = undefined
+esMalvado :: Personaje -> Bool 
+esMalvado personaje = 
+  (any (esDeTipo "Malvado") . elementos) personaje
+
+esDeTipo unTipo elemento = tipo elemento == unTipo
+
 
 danioQueProduce :: Personaje -> Elemento -> Float
-danioQueProduce = undefined
+danioQueProduce personaje elemento = 
+  salud personaje - salud (ataque elemento personaje)  
 
-enemigosMortales = undefined
+danioQueProduce' personaje elemento = 
+  ((salud personaje -) . salud . ataque elemento) personaje 
+
+enemigosMortales :: Personaje -> [Personaje] -> [Personaje]
+enemigosMortales personaje enemigos = 
+  filter (esEnemigoMortal personaje) enemigos
+
+esEnemigoMortal personaje enemigo =
+  (any (tieneAtaqueMortal personaje) . elementos) enemigo
+
+tieneAtaqueMortal personaje elemento =
+  danioQueProduce personaje elementos == salud personaje
+
+tieneAtaqueMortal' personaje elemento =
+  (estaMuerto . ataque elemento) personaje
+
+estaMuerto  = ((==0).salud) 
 
 -- Punto 3
 concentracion = undefined
