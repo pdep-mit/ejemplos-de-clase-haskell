e266d904c2d5a4416873945dcb184ab767a40f51
MARIANACECILIAMATOS <matosm@ar.ibm.com>
2019-05-21 17:35:57 -0300

    Agrego el ejemplo de uso que hicimos en la consola

--- clase7.hs
+++ clase7.hs
@@ -31,8 +31,8 @@ fantasma categoria asuntoPendiente =
       puedeDeshacerseDeElla = asuntoPendiente }
 
 enfrentarCriatura :: Persona -> Criatura -> Persona
-enfrentarCriatura persona criatura 
-    |gano persona criatura = 
+enfrentarCriatura persona criatura
+    |gano persona criatura =
         aumentarExperiencia (peligrosidad criatura) persona
     |otherwise = escaparse persona
 
@@ -48,17 +48,27 @@ escaparse :: Persona -> Persona
 escaparse persona = aumentarExperiencia 1 persona
 
 aumentarExperiencia :: Int -> Persona -> Persona
-aumentarExperiencia cantidad persona = 
+aumentarExperiencia cantidad persona =
     persona { nivelDeExperiencia = (nivelDeExperiencia persona) + cantidad}
-
---fantasma 3 (\persona -> edad persona < 13 && tieneItem "disfraz de oveja" persona)
 
 experienciaQuePuedeGanar :: Persona -> [Criatura] -> Int
 experienciaQuePuedeGanar persona listaDeCriaturas =
- (`subtract` (nivelDeExperiencia persona)) 
-    . nivelDeExperiencia 
+ (subtract (nivelDeExperiencia persona)) 
+    . nivelDeExperiencia
     . enfrentamientosSucesivos persona $ listaDeCriaturas
 
 enfrentamientosSucesivos :: Persona -> [Criatura] -> Persona
-enfrentamientosSucesivos =
-    foldl enfrentarCriatura 
+enfrentamientosSucesivos = foldl enfrentarCriatura
+
+{-
+Mostrar un ejemplo de consulta para el punto anterior incluyendo las siguientes criaturas:
+al siempredetras, a un grupo de 10 gnomos, un fantasma categoría 3 que requiere
+que la persona tenga menos de 13 años y un disfraz de oveja entre sus ítems
+para que se vaya y un fantasma categoría 1 que requiere que la persona tenga más de 10 de experiencia.
+-}
+
+{-
+experienciaQuePuedeGanar (UnaPersona 20 12 ["soplador de hojas", "disfraz de oveja", "diario"])
+  [siempreDetras, gnomos 10, fantasma 3 (\persona -> edad persona < 13 && tieneItem "disfraz de oveja" persona),
+  fantasma 1 ((>10).nivelDeExperiencia)]
+-}
