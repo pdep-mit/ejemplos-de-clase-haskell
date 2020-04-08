1330eaaa83a7f5ab8f8f8e09d9bd24ea628bb596
MARIANACECILIAMATOS <matosm@ar.ibm.com>
2020-04-08 19:02:34 -0300

    Código de clase 1 - 2020

--- src/Clase1.hs
+++ src/Clase1.hs
@@ -2,3 +2,23 @@ module Clase1 where
 
 doble :: Int -> Int
 doble numero = 2 * numero
+
+-- esMayor: dada una edad da true si es mayor o igual a 18
+esMayor :: Int -> Bool
+esMayor edad = edad >= 18
+
+-- esMenor: lo opuesto a esMayor
+esMenor :: Int -> Bool
+esMenor edad = not (esMayor edad)
+
+nombreFormateado :: String -> String -> String
+nombreFormateado nombre apellido = apellido ++ ", " ++ nombre
+
+conjuncion :: Bool -> Bool -> Bool
+conjuncion True True = True
+conjuncion _ _ = False
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n
+  | n > 0 = n * factorial (n-1)
